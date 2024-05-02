16821606b32655adb529c71c3ee890ea49f0c854
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-05-03 03:31:56 +0530

    Rate Limiting Some APIs

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/juju/ratelimit"
 	"github.com/sraynitjsr/controller"
 	"github.com/sraynitjsr/repository"
 	"github.com/sraynitjsr/service"
@@ -13,7 +13,7 @@ import (
 func main() {
 	db, err := repository.ConnectToDB("username:password@tcp(127.0.0.1:3306)/database_name")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer db.Close()
 
@@ -21,12 +21,24 @@ func main() {
 	userService := service.NewUserService(userRepo)
 	userController := controller.NewUserController(userService)
 
+	limiter := ratelimit.NewBucketWithRate(100, 100)
+
 	router := mux.NewRouter()
-	router.HandleFunc("/users", userController.GetUsers).Methods("GET")
-	router.HandleFunc("/users/{id}", userController.GetUser).Methods("GET")
+	router.HandleFunc("/users", limitMiddleware(userController.GetUsers, limiter)).Methods("GET")
+	router.HandleFunc("/users/{id}", limitMiddleware(userController.GetUser, limiter)).Methods("GET")
 	router.HandleFunc("/users", userController.CreateUser).Methods("POST")
 	router.HandleFunc("/users/{id}", userController.UpdateUser).Methods("PUT")
 	router.HandleFunc("/users/{id}", userController.DeleteUser).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	http.ListenAndServe(":8000", router)
+}
+
+func limitMiddleware(next http.HandlerFunc, limiter *ratelimit.Bucket) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if limiter.TakeAvailable(1) == 0 {
+			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
+			return
+		}
+		next(w, r)
+	}
 }
